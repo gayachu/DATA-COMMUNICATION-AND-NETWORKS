12060a523b252c2a8dda8a4bc64af67404c456d6
gayachu <110960970+gayachu@users.noreply.github.com>
2025-09-30 12:12:42 +0100

    Create tcp_echo_client.java

---
+++ TCP/tcp_echo_client.java
@@ -0,0 +1,43 @@
+package networks;
+
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class TCPEchoClient 
+{
+public static void main(String[] args) 
+{
+if (args.length< 2) return;
+String hostname = args[0];
+int port = Integer.parseInt(args[1]);
+Scanner console = new Scanner(System.in);
+try (Socket socket = new Socket(hostname, port)) 
+{
+OutputStream output = socket.getOutputStream();
+PrintWriter writer = new PrintWriter(output, true);
+String text;
+do 
+{
+System.out.println("Enter text :");
+text = console.nextLine();
+writer.println(text);
+InputStream input = socket.getInputStream();
+BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+String time = reader.readLine();
+System.out.println(time);
+} 
+while (!text.equals("bye"));
+socket.close();
+} 
+catch (UnknownHostException ex) 
+{
+System.out.println("Server not found: " + ex.getMessage());
+} 
+catch (IOException ex) 
+{
+System.out.println("I/O error: " + ex.getMessage());
+}
+}
+}
+
